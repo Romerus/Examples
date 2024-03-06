1eb139452e85c6131d4768e17bb04a50549664e4
Romerus <daniel.hager@outlook.at>
2024-03-06 11:54:28 +0100

    Update get-quarantine.ps1
    V2 - put it in function

--- get-quarantine.ps1
+++ get-quarantine.ps1
@@ -1,11 +1,14 @@
-Connect-ExchangeOnline
+#Connect-ExchangeOnline
 [int]$Daybefore = 0
 [int]$i = 0
 $Report = [System.Collections.Generic.List[Object]]::new(); $Now = Get-Date
 
-do {
-    $quarantine = Get-QuarantineMessage -PageSize 1000 -StartReceivedDate (get-date).AddDays($i).ToString("MM/dd/yyyy 00:00:00") -EndReceivedDate (get-date).AddDays($i).ToString("MM/dd/yyyy 23:59:59")
+function Get-AllQuarantine {
+    param (
+        [int]$pagecount
+    )
     
+    $quarantine = Get-QuarantineMessage -PageSize 1000 -StartReceivedDate (Get-Date).AddDays($i).ToString("MM/dd/yyyy 00:00:00") -EndReceivedDate (Get-Date).AddDays($i).ToString("MM/dd/yyyy 23:59:59") -Page $pagecount
     ForEach ($Message in $quarantine) {
    
         $RemainingTime = (New-TimeSpan -Start $Now -End $Message.Expires)
@@ -18,7 +21,7 @@ do {
             Else 
                {$Recipient = "; " + $Address }
        }
-        
+
         $ReportLine = [PSCustomObject]@{  #Update with details of what we have done
                 Identity         = $Message.Identity
                 Received         = Get-Date($Message.ReceivedTime) -format g
@@ -34,44 +37,17 @@ do {
                 MessageId = $Message.MessageId} 
          $Report.Add($ReportLine) 
          $ReportLine | Export-Csv -path "C:\temp\Quarantine.csv" -NoClobber -NoTypeInformation -Append  -Encoding UTF8 -Delimiter ","
-        
-         If($quarantine.count -gt 900)
-         {
-            $Zaehler = $quarantine.Count
-            Write-Host "zwischencount $Zaehler"
-            $quarantine = Get-QuarantineMessage -PageSize 1000 -StartReceivedDate (get-date).AddDays($i).ToString("MM/dd/yyyy 00:00:00") -EndReceivedDate (get-date).AddDays($i).ToString("MM/dd/yyyy 23:59:59") -Page 2
-            
-            ForEach ($Message in $quarantine) {
-   
-                $RemainingTime = (New-TimeSpan -Start $Now -End $Message.Expires)
-                $Remaining = $RemainingTime.Days.toString() + " days " + $RemainingTime.Hours.toString() + " hours"
-                [String]$Recipient = $Null; 
-                $Recipient = $Address
-                
-                $ReportLine = [PSCustomObject]@{  #Update with details of what we have done
-                        Identity         = $Message.Identity
-                        Received         = Get-Date($Message.ReceivedTime) -format g
-                        Recipient        = $Recipient
-                        Sender           = $Message.SenderAddress
-                        Subject          = $Message.Subject
-                        SenderDomain     = $Message.SenderAddress.Split("@")[1]
-                        Type             = $Message.QuarantineTypes
-                        Expires          = Get-Date($Message.Expires) -format g
-                        "Time Remaining" = $Remaining 
-                        ReleaseStatus = $Message.ReleaseStatus
-                        ReceivedTimeTest = $Message.ReceivedTime
-                        MessageId = $Message.MessageId} 
-                 $Report.Add($ReportLine) 
-                 $ReportLine | Export-Csv -path "C:\temp\Quarantine.csv" -NoClobber -NoTypeInformation -Append  -Encoding UTF8 -Delimiter ","}
-        }
-   }
+    }
+    return $quarantine
+}
 
-    $Zaehler = $quarantine.Count
-    Write-Output "Index: $i Q-count:$Zaehler"
+do {
+    $quarantine = Get-AllQuarantine -pagecount 1   
+    If($quarantine.count -gt 900) {$quarantine= get-AllQuarantine -pagecount 2}
+    
     $i=$i -1
-    $Daybefore=$Daybefore -1
+    $Daybefore=$Daybefore -1  
     
-    
-} until ($i -eq -34)
+} until ($i -eq -31)
 
 $Report.Count
